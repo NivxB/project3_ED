107bb8d26e999840aa9b61551c4678beda04c5ab
Daemon Aeon <carlos.morazan@unitec.edu>
2014-03-04 18:52:25 -0600

    first commit

---
+++ project3/src/project3/Project3.java
@@ -0,0 +1,20 @@
+
+package project3;
+
+/**
+ *
+ * @author carlos
+ */
+public class Project3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("--------------------CIUDADES--------------------");
+        System.out.println("1. Tegucigalpa\n2. Miami\n3. Cancun\n4. Atlanta\n5. New York\n6. Baltimore\n7. San Pedro Sula\n8. El Salvador");
+        System.out.println("9. Roatán\n10. Managua\n11. Ciudad de México\n12. Panamá\n13. Bogotá\n14. Lima\n15. Philadelphia\n16. Los Angeles");
+        System.out.println("17. Seattle\n18. Dallas\n19. Madrid\n20. Honolulu\n21. Berlin\n22. Tokyo\n23. Napoli\n24. Amsterdam");
+    }
+    
+}
